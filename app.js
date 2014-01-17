bc40dbc376783b68cb5a1deaa72b261f48618bf8
nmoya <nikmoy@gmail.com>
2014-01-17 13:14:24 -0200

    Testing socket.io

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
 var port = process.env.PORT || 5000;
-var http = require('http');
-var url = require('url');
 var express = require('express');
+var http = require('http');
 var app = express();
+var io = require('socket.io');
 
-//app.use(express.logger());
-app.use(express.static(__dirname + '/public'));
+var connectionCounter = 0;
+var serv_io = null;
 
-/* serves main page */
+/* Create server */
+server = http.createServer(app)
+server.listen(port, function () {
+    console.log("SERVER RUNNING. Port: " + port);
+});
+serv_io = io.listen(server);
+
+
+//Main Page
 app.get("/", function(req, res) {
    res.sendfile('index.html')
 });
 
 /* serves all the static files */
+app.use(express.static(__dirname + '/public'));
 app.get(/^(.+)$/, function(req, res){ 
  console.log('static file request : ' + req.params);
  res.sendfile( __dirname + req.params[0]); 
 });
-
-//app.post("/user/add", function(req, res) { 
-/* some server side logic */
-//res.send("OK");
-//});
- 
-
-http.createServer(app).listen(port, function () {
-    console.log("SERVER RUNNING. Port: " + port);
+app.post("/ggj/bcast", function(req, res) {
+    res.send("OK");
 });
 
+serv_io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+serv_io.sockets.on('connection', function (socket) { 
+    connectionCounter += 1;
+    console.log("Online: " + connectionCounter);
+})
+serv_io.sockets.on('disconnect', function (socket) { 
+    connectionCounter -= 1;
+    console.log("Online: " + connectionCounter);
+})
 
 /*app.configure(function(){
     app.use('/images', express.static(path.join(__dirname, '/images')));
---
+++ utils/notify.js
@@ -0,0 +1,64 @@
+//Docs: http://notifyjs.com/
+
+//Usage:
+    //gnotify("Hello World", "success");
+    //gnotify("Hello World", "warn");
+    //gnotify("Hello World", "info");
+    //gnotify("Hello World", "error");
+
+
+
+$.notify.defaults({ arrowShow: true });
+$.notify.defaults({ clickToHide: true });
+$.notify.defaults({ autoHide: true });
+$.notify.defaults({ autoHideDelay: 2500 });
+$.notify.defaults({ globalPosition: 'top right' });
+$.notify.defaults({ elementPosition: 'bottom left' });
+function gnotify (text, type) {
+    if (type == 'info')
+        $.notify(text, {className: type, globalPosition: "bottom left"} );
+    else
+        $.notify(text, type);
+};
+
+function enotify (element, text, type) {
+    $.notify(element, text, type);
+}
+
+
+
+
+
+
+
+/*
+{
+  // whether to hide the notification on click
+  clickToHide: true,
+  // whether to auto-hide the notification
+  autoHide: true,
+  // if autoHide, hide after milliseconds
+  autoHideDelay: 5000,
+  // show the arrow pointing at the element
+  arrowShow: true,
+  // arrow size in pixels
+  arrowSize: 5,
+  // default positions
+  elementPosition: 'bottom left',
+  globalPosition: 'top right',
+  // default style
+  style: 'bootstrap',
+  // default class (string or [string])
+  className: 'error',
+  // show animation
+  showAnimation: 'slideDown',
+  // show animation duration
+  showDuration: 400,
+  // hide animation
+  hideAnimation: 'slideUp',
+  // hide animation duration
+  hideDuration: 200,
+  // padding between element and notification
+  gap: 2
+}
+*/
