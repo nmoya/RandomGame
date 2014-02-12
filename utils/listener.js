a645553ae1b274425066490ae86bd8e06865acd8
nmoya <nikmoy@gmail.com>
2014-02-12 01:36:20 -0200

    New attempt

--- utils/listener.js
+++ utils/listener.js
@@ -25,7 +25,7 @@ function listen()
     });
 
     socket.on("user_disconnected", function (user_id) {
-        if (UserList[user_id].obj)
+        if (typeof UserList[user_id].obj != "undefined")
         {
             Stage.removeChild(UserList[user_id].obj);
             last_user_removed = user_id;
